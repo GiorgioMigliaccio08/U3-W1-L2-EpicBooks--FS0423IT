dd36bb495baf354db7d52c06fe4f8db5fdb0e308
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-24 15:22:09 +0200

    Add MyNav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importo la mia NavBar
+import CustomNavbar from "./components/MyNav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CustomNavbar additionalText="I migliori libbri del web!" />
     </div>
   );
 }
