7ad95e46d7c8ab618d279bc06abd3a3268d05e5a
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-26 16:20:45 +0200

    AddComment jsx

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,28 +1,35 @@
-import CommentList from "./CommentList"
+import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 
-import {col , Container , Row } from "react-bootstrap"
+import { col, Container, Row } from "react-bootstrap";
 
-
-class CommentArea extends Component{
- state = {
-    comments: [], 
- }
- getComments = async () => {
+class CommentArea extends Component {
+  state = {
+    comments: [],
+  };
+  getComments = async () => {
     try {
-        const res = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.bookId,{
-            headers:{
-                Authorization:keyApi,
-            },
-
+      const res = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.bookId,
+        {
+          headers: {
+            Authorization: keyApi,
+          },
         }
-        )
-        if(res.ok){
-            const data = await res.json()
-            console.log('data',data)
+      );
+      if (res.ok) {
+        const data = await res.json();
+        console.log("data", data);
 
-        }
+        this.setState({
+          comments: data,
+        });
+      } else {
+        throw new Error("Fetch non conclusa");
+      }
+    } catch (error) {
+      console.log("errore nel caricamento dei commenti", error);
     }
-
- }
+  };
 }
