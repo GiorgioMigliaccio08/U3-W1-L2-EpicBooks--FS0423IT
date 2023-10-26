625f4c56fcf0c05da206a9cdcd55326e2bb2d2be
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-26 15:27:00 +0200

    Add  CommentArea.jsx

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,17 @@
+import CommentList from "./CommentList"
+import AddComment from "./AddComment";
+
+import {col , Container , Row };
+
+
+class CommentArea extends Component{
+ state = {
+    comments: [], 
+ }
+ getComments = async () => {
+    try {
+        const res = await fetch('')
+    }
+
+ }
+}
