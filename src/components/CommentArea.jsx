146e807ec14ccc57627c2c5586e758be7d346841
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-26 15:32:50 +0200

    Add CommentsList.jsx

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,7 +1,7 @@
 import CommentList from "./CommentList"
 import AddComment from "./AddComment";
 
-import {col , Container , Row };
+import {col , Container , Row } from "react-bootstrap"
 
 
 class CommentArea extends Component{
@@ -10,7 +10,18 @@ class CommentArea extends Component{
  }
  getComments = async () => {
     try {
-        const res = await fetch('')
+        const res = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.bookId,{
+            headers:{
+                Authorization:keyApi,
+            },
+
+        }
+        )
+        if(res.ok){
+            const data = await res.json()
+            console.log('data',data)
+
+        }
     }
 
  }
