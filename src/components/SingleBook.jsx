265eb0df884f0b22d17811953dd5a4ec811e9d61
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-31 14:43:17 +0100

    Commit 4

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,29 +1,41 @@
 import { Component } from "react";
 import { Card } from "react-bootstrap";
+// import CommentArea from './CommentArea'
 
 class SingleBook extends Component {
   state = {
     selected: false,
   };
 
-  toggleSelected = () => {
-    this.setState({
-      selected: !this.state.selected,
-    });
-  };
+  // ogni SingleBook riceve this.props.selectedAsin, ovvero l'asin del libro corrente selezionato
+  // per fare in modo che solo UN SingleBook riceva il bordo rosso, dobbiamo comparare il proprio asin
+  // con il selectedAsin.
 
   render() {
     return (
-      <Card className={this.state.selected ? "red-border" : ""}>
-        <Card.Img
-          variant="top"
-          src={this.props.book.img}
-          onClick={this.toggleSelected}
-        />
-        <Card.Body>
-          <Card.Title>{this.props.book.title}</Card.Title>
-        </Card.Body>
-      </Card>
+      <>
+        <Card
+          onClick={() => {
+            this.setState({ selected: !this.state.selected });
+            this.props.changeAsin(this.props.book.asin);
+          }}
+          // style={{ border: this.state.selected ? '3px solid red' : 'none' }}
+          style={{
+            border:
+              this.props.book.asin === this.props.selectedAsin
+                ? "3px solid red"
+                : "none",
+          }}
+        >
+          <Card.Img variant="top" src={this.props.book.img} />
+          <Card.Body>
+            <Card.Title style={{ color: "black" }}>
+              {this.props.book.title}
+            </Card.Title>
+          </Card.Body>
+        </Card>
+        {/* {this.state.selected && <CommentArea bookId={this.props.book.asin} />} */}
+      </>
     );
   }
 }
